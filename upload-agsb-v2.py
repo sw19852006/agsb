b25af981b73118122abd205323ac872e98ebaeaa
sanhuan2009 <32083961+sw19852006@users.noreply.github.com>
2025-06-07 16:03:04 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -34,7 +34,7 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 USER_NAME = "sanhuan2009"         # 用户名
 UUID = "8f79ce53-1a74-4640-90b7-90928fee268b"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "agsb.xuehua2009.dpdns.org/"                   # 域名，留空则自动获取
+DOMAIN = "agsb.xuehua2009.dpdns.org"                   # 域名，留空则自动获取
 CF_TOKEN = "eyJhIjoiNWM1YTAxYzRmNTdiM2MwYzg0ZDNhOWI3NGUyZmY3ZDkiLCJ0IjoiNTUyY2M1OWUtMDIxYi00ZWZhLWEwNmQtNDEzYjFlZGY2MDkxIiwicyI6Ik1tWTFZakl4TUdVdE1XUmtaaTAwWm1VM0xXSXpPV1V0TkRReE56a3dNREJrWm1JMCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
